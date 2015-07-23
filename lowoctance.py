d73fcb271f0f96745841ca6a969aebd58b345368
akbarq <breakpoint.py@gmail.com>
2015-07-23 00:20:12 -0500

    Update lowoctance.py

--- lowoctance.py
+++ lowoctance.py
@@ -9,16 +9,16 @@ import signal
 import os
 
 print"""
-####################################################################################################
-# Description: LowOctane honeypot mimics Veeder-root Automatic Tank Gauge (ATG) systems.           #
-#	       The honeypot listens on TCP port 10001 and only responds to fucntion code "I20100". #
-#              All other function code requests from the client result in "9999FF1B" which means   #
-#	       function code not recognized.							   #
+#####################################################################################################
+# Description: LowOctane honeypot mimics Veeder-root Automatic Tank Gauge (ATG) systems.            #
+#	       The honeypot listens on TCP port 10001 and only responds to fucntion code "I20100".  #
+#              All other function code requests from the client result in "9999FF1B" which means    #
+#	       function code not recognized.							    #
 # 	       This honeypot can be used to study attacks against ATG systems e.g. malicious use of #
-#	       function codes against ATG systems. 						   #
-# Version: 1.0											   #
-# Author: Akbar Qureshi										   #
-####################################################################################################
+#	       function codes against ATG systems. 						    #
+# Version: 1.0											    #
+# Author: Akbar Qureshi										    #
+#####################################################################################################
 """
 
 
